7b6debb92858e89cb9d5b0a660dd5699cc998ae9
Diego <diegodevitte@gmail.com>
2021-02-27 16:30:13 -0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      
+      $this->call(RolesAndPermissionsSeeder::class);
+      $this->call(SuperAdminUserSeeder::class);
     }
 }
